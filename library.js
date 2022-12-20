2c2883d3083a5cad9e16b0fb8b57f5beabdff277
Alexxanderr20 <50477848+Alexxanderr20@users.noreply.github.com>
2022-12-20 12:25:43 -0600

    Adding files via upload

---
+++ library.js
@@ -0,0 +1,20 @@
+let myLibrary = [];
+
+function Book()
+{
+    this.author = "";
+    this.title = "";
+}
+
+function addBookToLibrary()
+{
+    // myLibrary = prompt("Please enter a book: ");
+    // console.log(myLibrary);
+}
+//     for(let i = 0; i < myLibrary.length; i++)
+//     {
+//             console.log(myLibrary[i]);
+//     }
+// }
+
+addBookToLibrary();
